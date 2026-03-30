7702b3429f9621ec61e83a5ff1f8c684ed4b06b0
jgs-jeeves <jeeves@johngalt.id>
2026-03-30 14:49:07 +0000

    [51] chore: remove remaining temp scripts

--- cleanup.cjs
+++
@@ -1,8 +0,0 @@
-const {execSync} = require('child_process');
-const fs = require('fs');
-const cwd = 'D:/repos/karmaniverous/jeeves';
-fs.unlinkSync(cwd + '/commit.cjs');
-fs.unlinkSync(cwd + '/create-pr.cjs');
-execSync('git add -A', {cwd, stdio: 'inherit'});
-execSync('git commit -m "chore: remove temp scripts"', {cwd, stdio: 'inherit'});
-execSync('git push', {cwd, stdio: 'inherit'});
